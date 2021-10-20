adb8ed368ec40b61b117514b245f20a0cc458a3c
Maximiliano Diaz <maximiliano_diaz@outlook.com.ar>
2021-10-20 13:24:11 -0300

    Add generatePopupContent function.

---
+++ src/comment-content.js
@@ -0,0 +1,74 @@
+const popup = document.getElementById('popup');
+const { backgroundColor } = document.body.style;
+
+const generatePopupContent = (tvShow, commentList) => {
+  backgroundColor = 'rgba(0,0,0,0.6)';
+  popup.innerHTML = '';
+
+  const container = document.createElement('div');
+  container.classList.add('popup-container');
+
+  const innerContent = document.createElement('div');
+  innerContent.classList.add('inner-content');
+
+  const imageClose = document.createElement('div');
+  imageClose.classList.add('photo-close');
+
+  const showCover = document.createElement('img');
+  showCover.classList.add('show-cover');
+  showCover.src = tvShow.image.medium;
+
+  const closeButton = document.createElement('button');
+  const closeIcon = document.createElement('i');
+  closeIcon.classList.add('fas fa-times fa-2x');
+
+  closeButton.appendChild(closeIcon);
+
+  imageClose.appendChild(showCover, closeButton);
+
+  const descriptionContainer = document.createElement('div');
+  descriptionContainer.classList.add('description-container');
+
+  const descriptionHeader = document.createElement('h2');
+  descriptionHeader.innerText = tvShow.name;
+
+  const descriptionOne = document.createElement('div');
+  descriptionOne.classList.add('description-1');
+
+  const network = document.createElement('p');
+  network.innerText = tvShow.network.name;
+
+  const language = document.createElement('p');
+  language.innerText = tvShow.language;
+
+  descriptionOne.appendChild(network, language);
+
+  const descriptionTwo = document.createElement('div');
+  descriptionOne.classList.add('description-2');
+
+  const rating = document.createElement('p');
+  rating.innerText = tvShow.rating.average;
+
+  const genre = document.createElement('p');
+  genre.innerText = tvShow.genres[0];
+
+  descriptionTwo.appendChild(rating, genre);
+
+  descriptionContainer.appendChild(descriptionHeader, descriptionOne, descriptionTwo);
+
+  const commentsContainer = document.createElement('div');
+  commentsContainer.classList.add('comments-container');
+
+  const commentsHeader = document.createElement('h3');
+  commentsHeader.innerText = `Comments: ${commentList.length}`;
+
+  commentsContainer.appendChild(commentsHeader,
+    commentList.map((comment) => {
+      const commentItem = document.createElement('p');
+      commentItem.innerText = `${comment.creation_date} ${comment.username} ${comment.comment}`;
+      return commentItem;
+    }),
+  );
+};
+
+export default generatePopupContent;
--- src/comments.js
+++ src/comments.js
@@ -1,9 +1,12 @@
 import { getTvShowById } from './tvShowsApi.js';
 import { getComments } from './comments-api.js';
+import generatePopupContent from './comment-content.js';
 
 const displayCommentPopup = async (id) => {
   const tvShow = await getTvShowById(id);
   const commentList = await getComments(id);
 
-  //generatePopupContent(tvShow, commentList);
+  generatePopupContent(tvShow, commentList);
 };
+
+export default displayCommentPopup;
